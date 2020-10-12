ef95116d03d409a4a4a93090b75cb30519943992
MFukazawa <micahfukazawa@gmail.com>
2020-10-12 18:06:52 +0900

    [Update] Add all tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,53 @@
 /* eslint @typescript-eslint/explicit-function-return-type: 0 */
 
-import { add } from './index';
+import { shi, kyan } from './index'
 
-describe('add', () => {
-  test('should add 2 numbers', () => {
-    expect(add(1, 2)).toEqual(3);
+describe('catTests', () => {
+  beforeEach(() => {
+    shi.hungerPoint = 0;
+    kyan.hungerPoint = 0;
+  })
+
+  test('【条件】しーちゃん、5回遊んだ後に空腹確認→【expected】「まだ大丈夫」', () => {
+    for (let i = 0; i < 5; i++) {
+      shi.play()
+    }
+    expect(shi.isHungry).toBe(false);
   });
-});
+  
+  test('【条件】しーちゃん、9回遊んだ後に空腹確認→【expected】「ハラヘリ」', () => {
+    for (let i = 0; i < 9; i++) {
+      shi.play()
+    }
+    expect(shi.isHungry).toBe(true);
+  });
+  
+  test('【条件】しーちゃん、10回遊んだ後に空腹確認→【expected】暴れる', () => {
+    for (let i = 0; i < 10; i++) {
+      shi.play()
+    }
+    expect(shi.isBerserk).toBe(true);
+  });
+
+  test('【条件】キャンちゃん、2回遊んだ後に空腹確認→【expected】「まだ大丈夫」', () => {
+    for (let i = 0; i < 2; i++) {
+      kyan.play()
+    }
+    expect(kyan.isHungry).toBe(false);
+  });
+
+  test('【条件】キャンちゃん、3回遊んだ後に空腹確認→【expected】「まだ大丈夫」', () => {
+    for (let i = 0; i < 3; i++) {
+      kyan.play()
+    }
+    expect(kyan.isHungry).toBe(true);
+  });
+
+  test('【【条件】キャンちゃん、5回遊んだ後に空腹確認→【expected】暴れる', () => {
+    for (let i = 0; i < 5; i++) {
+      kyan.play()
+    }
+    expect(kyan.isBerserk).toBe(true);
+  });  
+})
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,92 @@
-export const add = (num1: number, num2: number): number => {
-  return num1 + num2;
-};
+const MINIMUM_HUNGER = 0;
+
+// CatOwner
+export class CatOwner {
+  public checkHunger(cat: ICat) {
+    // check hungerPoint
+    cat.meow();
+  }
+
+  public feed(snack: string, cat: Cat) {
+    // おやつかシーチキン
+    if (snack === 'oyatsu') {
+      cat.eat('oyatsu');
+    } else if (snack === 'tuna') {
+      cat.eat('tuna');
+    } else return;
+  }
+}
+
+// 猫
+interface ICat {
+  name: string;
+  hungerPoint: number;
+  hungerCoefficient: number;
+  isHungry: boolean;
+  isBerserk: boolean;
+  eat(snack: string): void;
+  meow(): void;
+  play(): void;
+}
+
+export class Cat implements ICat {
+  public name: string;
+  public hungerPoint: number; // 略称 HP
+  public hungerCoefficient: number;
+
+  constructor(name: string, hungerPoint: number, hungerCoefficient: number) {
+    this.name = name;
+    this.hungerPoint = MINIMUM_HUNGER;
+    this.hungerCoefficient = hungerCoefficient;
+  }
+
+  get isHungry(): boolean {
+    return this.hungerPoint >= 60;
+  }
+
+  get isBerserk(): boolean {
+    // HP >= 100 ? 何をしようとしても暴れる
+    return this.hungerPoint >= 100;
+  }
+
+  public meow() {
+    this.isHungry ? console.log('腹減ったニャン') : console.log('おやついらん');
+  }
+
+  public eat(snack: string) {
+    // isBerserk ? 食べない
+    if (this.isBerserk) return console.log('!#$%^*#^^&$%^$)&*%^%$@#');
+
+    // おやつ HP回復
+    const snackHP = 40;
+
+    // おやつ　HP <= snackHP
+    if (snack === 'oyatsu' && this.hungerPoint <= snackHP) {
+      return (this.hungerPoint = 0);
+    }
+
+    // おやつ　HP > snackHP
+    if (snack === 'oyatsu') {
+      return (this.hungerPoint -= snackHP);
+    }
+
+    // シーチキン HP = 0
+    else if (snack === 'tuna') {
+      return (this.hungerPoint = 0);
+    } else return;
+  }
+
+  public play() {
+    // isBerserk ? 遊ばない
+    if (this.isBerserk) return console.log('!#$%^*#^^&$%^$)&*%^%$@#');
+
+    return (this.hungerPoint += 10 * this.hungerCoefficient);
+  }
+}
+
+// CatOwner
+export const owner: CatOwner = new CatOwner();
+
+// 猫２匹 ・1. しーちゃん、  2.キャンちゃん
+export const shi: ICat = new Cat('Shi', 0, 1);
+export const kyan: ICat = new Cat('Kyan', 0, 2);
